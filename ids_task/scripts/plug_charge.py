d3c7040d1db16a2715bd0ac3458822650b27aba9
suneric <syf_more@163.com>
2022-09-15 16:01:47 -0400

    plug with random adjustment

--- ids_task/scripts/plug_charge.py
+++ ids_task/scripts/plug_charge.py
@@ -155,13 +155,69 @@ class AutoChagerTask:
         self.ftsensor = FTSensor('/tf_sensor_hook')
         self.contact = BumpSensor('/bumper_plug')
         self.info = None
+        self.info_count = 0
         self.target = None
 
     def detect_cb(self,info):
+        self.info_count += 1
         self.info = info
 
     def status(self):
         return self.task_status
+
+    def detect_info(self, type, size=3):
+        rate = rospy.Rate(10)
+        info_count = self.info_count
+        c,l,r,t,b,x,y,z,nx,ny,nz=[],[],[],[],[],[],[],[],[],[],[]
+        while len(c) < size:
+            if self.info_count <= info_count or self.info.type != type or self.info.c < 0.5:
+                continue
+
+            if len(c) == 0: # first detected
+                c.append(self.info.c)
+                l.append(self.info.l)
+                r.append(self.info.r)
+                t.append(self.info.t)
+                b.append(self.info.b)
+                x.append(self.info.x)
+                y.append(self.info.y)
+                z.append(self.info.z)
+                nx.append(self.info.nx)
+                ny.append(self.info.ny)
+                nz.append(self.info.nz)
+            else: # check if it is the same target
+                rcu,rcv = (l[0]+r[0])/2, (t[0]+b[0])/2
+                cu,cv = (self.info.l+self.info.r)/2,(self.info.t+self.info.b)/2
+                if abs(cu-rcu) < 3 and abs(cv-rcv) < 3:
+                    c.append(self.info.c)
+                    l.append(self.info.l)
+                    r.append(self.info.r)
+                    t.append(self.info.t)
+                    b.append(self.info.b)
+                    x.append(self.info.x)
+                    y.append(self.info.y)
+                    z.append(self.info.z)
+                    nx.append(self.info.nx)
+                    ny.append(self.info.ny)
+                    nz.append(self.info.nz)
+            info_count = self.info_count
+            rate.sleep()
+        # create a info based on the average value
+        info = DetectionInfo()
+        info.detectable = True
+        info.type = type
+        info.c = np.mean(c)
+        info.l = np.mean(l)
+        info.r = np.mean(r)
+        info.t = np.mean(t)
+        info.b = np.mean(b)
+        info.x = np.mean(x)
+        info.y = np.mean(y)
+        info.z = np.mean(z)
+        info.nx = np.mean(nx)
+        info.ny = np.mean(ny)
+        info.nz = np.mean(nz)
+        return info
 
     def prepare(self):
         self.task_status = "preparing"
@@ -170,102 +226,149 @@ class AutoChagerTask:
         self.task_status = "prepared"
 
     def searching(self):
-        def detect_info(type):
+        """
+        get size of target type detected info, and average the values
+        """
+        def search_target(type,vel=1.0):
+            print("searching target type {}".format(type))
+            self.driver.stop()
             rate = rospy.Rate(10)
             while self.info == None or self.info.type != type:
+                self.driver.drive(vel,0.0)
                 rate.sleep()
-            print(self.info)
-            return self.info
+            self.driver.stop()
+            return self.detect_info(type=type,size=1)
 
-        def align_endeffector(x,y):
-            print("aligning endeffector to ({:.4f},{:.4f})".format(x,y))
+        def align_endeffector(detect):
+            detect = self.detect_info(type=detect.type,size=3)
+            print("aligning endeffector to ({:.4f},{:.4f})".format(detect.x,detect.y))
             pos = self.fdController.hslider_pos()
-            self.fdController.move_hslider(pos - x)
+            self.fdController.move_hslider(pos - detect.x)
             pos = self.fdController.vslider_height()
-            self.fdController.move_vslider(pos - y + 0.0725)
+            self.fdController.move_vslider(pos - detect.y + 0.072)
+            return self.detect_info(type=detect.type,size=3)
 
         def align_normal(info, tolerance=0.01):
             print("adjusting orientation to nx < {:.4f}".format(tolerance))
             detect = info
             rate = rospy.Rate(10)
             while abs(detect.nx) > tolerance:
-                self.driver.drive(0.0,np.sign(detect.nx)*0.2)
-                if self.info and self.info.type == detect.type:
-                    detect = self.info
-                    print(" === outlet normal: ({:.4f},{:.4f},{:.4f})".format(detect.nx,detect.ny,detect.nz))
+                self.driver.drive(0.0,np.sign(detect.nx)*0.5)
+                detect = self.detect_info(type=info.type,size=1)
+                print(" === normal: ({:.4f},{:.4f},{:.4f})".format(detect.nx,detect.ny,detect.nz))
                 rate.sleep()
             self.driver.stop()
-            return detect
+            return search_target(type=info.type,vel=0.2)
 
-        def move_closer(info, target=0.8):
-            print("moving closer to {:.4f}".format(target))
+        def move_closer(info, distance=0.8):
+            print("moving closer to {:.4f}".format(distance))
             detect = info
             rate = rospy.Rate(10)
-            while detect.z > target:
-                self.driver.drive(1.0,0.0)
-                if self.info and self.info.type == detect.type:
-                    detect = self.info
-                    print(" === outlet position: ({:.4f},{:.4f},{:.4f})".format(detect.x,detect.y,detect.z))
+            while detect.z > distance:
+                self.driver.drive(0.5,0.0)
+                detect = self.detect_info(type=info.type,size=1)
+                print(" === position: ({:.4f},{:.4f},{:.4f})".format(detect.x,detect.y,detect.z))
                 rate.sleep()
             self.driver.stop()
-            return detect
+            return self.detect_info(type=info.type,size=3)
 
-        self.driver.stop()
+        def approach(info, distance=0.3):
+            d0 = info.z
+            w0 = info.r-info.l
+            h0 = info.b-info.t
+            u0 = (info.l+info.r)/2
+            v0 = (info.t+info.b)/2
+            print(" === initial distance: {:.4f},{:.4f},{:.4f}".format(d0, w0, h0))
+            d = d0
+            while d > distance:
+                self.driver.drive(0.5,0.0)
+                detect = self.detect_info(type=4,size=1)
+                w = detect.r-detect.l
+                h = detect.b-detect.t
+                d = (0.5*(w0/w)+0.5*(h0/h))*d0
+                print(" === estimated dsiatnce: {:.4f},{:.4f},{:.4f}".format(d, w, h))
+            self.driver.stop()
+            return self.detect_info(type=4,size=1)
+
         self.task_status = "searching"
-        print("searching outlet...")
-        rate = rospy.Rate(10)
-        while self.info == None or self.info.type != 3:
-            self.driver.drive(-1.0,0.0)
-            rate.sleep()
-        detect = self.info
-        self.driver.stop()
-
+        detect = search_target(type=4,vel=-1.0)
         detect = align_normal(detect,tolerance=0.005)
-        align_endeffector(detect.x,detect.y)
-        detect = detect_info(type=3)
-        detect = move_closer(detect, target=0.8)
-
-        print("searching socket hole...")
-        detect = detect_info(type=4)
+        detect = align_endeffector(detect)
+        detect = move_closer(detect,distance=0.8)
         detect = align_normal(detect,tolerance=0.005)
-        align_endeffector(detect.x,detect.y)
-        self.target = detect_info(type=4)
+        detect = align_endeffector(detect)
+        self.target = approach(detect,distance=0.2)
         print(self.target)
         self.task_status = "ready"
 
     def plugin(self):
-        print("plugin to charge...")
-        self.task_status == "plugin"
-
-        self.fdController.move_plug(0.05)
-        self.fdController.plug.lock()
-
-        forces = self.ftsensor.forces()
-        print("Force Sensor 1: detected forces [x, y, z]", forces)
-        while not self.contact.connected():
-            # self.fdController.vslider.lock()
-            # self.fdController.hslider.lock()
-            self.driver.drive(0.5,0)
-            rospy.sleep(0.01)
+        def plug():
+            self.fdController.vslider.lock()
+            self.fdController.hslider.lock()
+            success = False
             forces = self.ftsensor.forces()
             print("Force Sensor 1: detected forces [x, y, z]", forces)
-            if abs(forces[0]) > 5:
-                self.driver.drive(-0.5,0)
-                rospy.sleep(0.1)
-                self.driver.stop()
-                # self.fdController.vslider.unlock()
-                # self.fdController.hslider.unlock()
-                posh = self.fdController.hslider_pos()
-                posv = self.fdController.vslider_height()
-                rad = np.random.uniform(size=2)
-                dh = 0.001*(rad[0]-0.5)
-                dv = 0.001*(rad[1]-0.5)
-                self.fdController.move_hslider(posh+dh)
-                self.fdController.move_vslider(posv+dv)
+            rate = rospy.Rate(10)
+            while not success and forces[0] > -10 and abs(forces[1]) < 0.5 and abs(forces[2]) < 0.25:
+                self.driver.drive(1.0,0)
+                rate.sleep()
+                forces = self.ftsensor.forces()
+                print("Force Sensor 1: detected forces [x, y, z]", forces)
+                success = self.contact.connected()
+            self.driver.stop()
+            self.fdController.vslider.unlock()
+            self.fdController.hslider.unlock()
+            return success
 
+        def leave(vel=-0.2):
+            w0 = self.target.r-self.target.l
+            h0 = self.target.b-self.target.t
+            detect = None
+            w,h = w0+10,h0+10
+            while not detect or w > w0 or h > h0:
+                self.driver.drive(vel,0.0)
+                detect = self.detect_info(type=4,size=1)
+                w = detect.r-detect.l
+                h = detect.b-detect.t
+            self.driver.stop()
+
+        def adjust(hpos,vpos):
+            # cu = (self.target.r+self.target.l)/2
+            # cv = (self.target.t+self.target.b)/2
+            # # horizontal adjust
+            # detect = self.detect_info(type=4,size=1)
+            # uerr = cu-(detect.l+detect.r)/2
+            # print("pixel errer u:",uerr,cu, (detect.l+detect.r)/2)
+            # while abs(uerr) > 2:
+            #     self.fdController.move_hslider(hpos+0.001*np.sign(uerr))
+            #     detect = self.detect_info(type=4,size=1)
+            #     uerr = cu-(detect.l+detect.r)/2
+            #     print("pixel errer u:", uerr,cu,(detect.l+detect.r)/2)
+            # vertical adjust
+            dh = 0.002*(np.random.uniform()-0.5)
+            self.fdController.move_vslider(vpos+dh)
+            dv = 0.002*(np.random.uniform()-0.5)
+            self.fdController.move_hslider(hpos+dv)
+
+        print("plugin to charge...")
+        self.task_status == "plugin"
+        self.fdController.move_plug(0.03)
+        self.fdController.plug.lock()
+        hpos = self.fdController.hslider_pos()
+        vpos = self.fdController.vslider_height()
+        success, numOfTry = False, 0
+        while numOfTry < 30:
+            numOfTry += 1
+            success = plug()
+            if not success:
+                # leave(vel=-0.5)
+                adjust(hpos,vpos)
+            else:
+                break
         self.driver.stop()
-        self.task_status = "done"
         self.fdController.plug.unlock()
+        print("plugin finished {} with {} tries".format(success, numOfTry))
+        self.task_status = "done"
 
 ## transformations
 def quaternion_pose(x,y,yaw):
